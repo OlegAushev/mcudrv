78f26b52cd58c1b057a0ce664d9865a5e9ff1359
Oleg Aushev <aushevom@gmail.com>
2023-07-20 15:47:17 +0300

    add F4 def and system

---
+++ f4/mcu_def.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+#ifdef STM32F4xx
+
+#include "stm32f4xx.h"
+#include <cassert>
+
+
+namespace mcu {
+
+using HalStatus = HAL_StatusTypeDef;
+
+
+class InterruptPriority {
+private:
+    uint32_t _value;
+public:
+    explicit InterruptPriority(uint32_t value)
+            : _value(value) {
+        assert_param(value <= 15);
+    }
+
+    uint32_t get() const { return _value; }
+};
+
+} // namespace mcu
+
+#endif
+
---
+++ f4/system/system.h
@@ -0,0 +1,72 @@
+#pragma once
+
+
+#ifdef STM32F4xx
+
+#include "../mcu_def.h"
+#include <emblib_stm32/core.h>
+#include <cstdio>
+#include <chrono>
+
+
+//void MX_GPIO_Init();
+
+
+namespace mcu {
+
+void init_hal();
+
+
+void init_device_clk();
+
+
+inline uint32_t core_clk_freq() { return SystemCoreClock; }
+
+
+void reset_device();
+
+
+void fatal_error(const char* hint, int code = 0);
+
+
+inline void delay_ms(std::chrono::milliseconds delay) {
+    HAL_Delay(static_cast<uint32_t>(delay.count()));
+}
+
+
+inline void enable_interrupts() { __enable_irq(); }
+
+
+inline void disable_interrupts() { __disable_irq(); }
+
+
+class critical_section {
+private:
+    bool irq_enabled;
+public:
+    critical_section() {
+        irq_enabled = (__get_PRIMASK() == 0);
+        __disable_irq();
+    }
+
+    ~critical_section() {
+        if (irq_enabled) {
+            __enable_irq();
+        }
+    }
+};
+
+
+// inline float calculate_mcu_temperature(uint32_t adcData) {
+//     return float(int32_t(adcData) - int32_t(*TEMPSENSOR_CAL1_ADDR)) * (float(TEMPSENSOR_CAL2_TEMP) - float(TEMPSENSOR_CAL1_TEMP))
+//             / (int32_t(*TEMPSENSOR_CAL2_ADDR) - int32_t(*TEMPSENSOR_CAL1_ADDR)) + float(TEMPSENSOR_CAL1_TEMP); 
+// }
+
+
+// inline float calculate_mcu_vref(uint32_t adcData) {
+//     return 3.3f * float(*VREFINT_CAL_ADDR) / float(adcData);
+// }
+
+} // namespace mcu
+
+#endif
