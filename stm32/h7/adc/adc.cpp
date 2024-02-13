d2815c1ab7e193c660f135f6c8918ab25cc5772a
Oleg Aushev <aushevom@gmail.com>
2024-02-13 13:40:46 +0300

    update h7-adc driver

--- stm32/h7/adc/adc.cpp
+++ stm32/h7/adc/adc.cpp
@@ -12,12 +12,14 @@ namespace mcu {
 namespace adc {
 
 
-Module::Module(Peripheral peripheral, const Config& config)
+Module::Module(Peripheral peripheral, const Config& config, dma::Stream* dma)
         : emb::interrupt_invoker_array<Module, peripheral_count>(this, std::to_underlying(peripheral))
-        , _peripheral(peripheral) {
-    enable_clk();
+        , _peripheral(peripheral)
+        , _reg(impl::adc_instances[std::to_underlying(peripheral)])
+        , _reg_common(impl::adc_common_instances[std::to_underlying(peripheral)]) {
+    _enable_clk(_peripheral);
     _handle.Instance = impl::adc_instances[std::to_underlying(_peripheral)];
-    _handle.Init = config.hal_init;
+    _handle.Init = config.hal_config;
 
     if (HAL_ADC_Init(&_handle) != HAL_OK) {
         fatal_error("ADC module initialization failed");
@@ -33,23 +35,43 @@ void Module::calibrate() {
 }
 
 
-void Module::add_regular_channel(PinConfig pin_config, ChannelConfig channel_config) {
-    mcu::gpio::Config cfg = {};
-    cfg.port = pin_config.port;
-    cfg.pin.Pin = pin_config.pin;
-    cfg.pin.Mode = GPIO_MODE_ANALOG;
-    cfg.pin.Pull = GPIO_NOPULL;
-    mcu::gpio::AnalogPin input(cfg);
+// void Module::initialize_regular_channel(PinConfig pin_config, ChannelConfig channel_config) {
+//     mcu::gpio::Config cfg = {};
+//     cfg.port = pin_config.port;
+//     cfg.pin.Pin = pin_config.pin;
+//     cfg.pin.Mode = GPIO_MODE_ANALOG;
+//     cfg.pin.Pull = GPIO_NOPULL;
+//     mcu::gpio::AnalogPin input(cfg);
 
-    if (HAL_ADC_ConfigChannel(&_handle, &channel_config.hal_init) != HAL_OK) {
-        fatal_error("ADC regular channel initialization failed");
-    }
-}
+//     if (HAL_ADC_ConfigChannel(&_handle, &channel_config.hal_config) != HAL_OK) {
+//         fatal_error("ADC regular channel initialization failed");
+//     }
+// }
 
 
-void Module::add_internal_channel(ChannelConfig channel_config) {
-    if (HAL_ADC_ConfigChannel(&_handle, &channel_config.hal_init) != HAL_OK) {
-        fatal_error("ADC internal channel initialization failed");
+// void Module::initialize_internal_channel(ChannelConfig channel_config) {
+//     if (HAL_ADC_ConfigChannel(&_handle, &channel_config.hal_config) != HAL_OK) {
+//         fatal_error("ADC internal channel initialization failed");
+//     }
+// }
+
+
+void Module::_enable_clk(Peripheral peripheral) {
+    switch (peripheral) {
+    case Peripheral::adc1:
+    case Peripheral::adc2:
+        if (_clk_enabled[0]) return;
+        __HAL_RCC_ADC12_CLK_ENABLE();
+        _clk_enabled[0] = true;
+        break;
+    case Peripheral::adc3:
+        if (_clk_enabled[1]) return;
+        __HAL_RCC_ADC3_CLK_ENABLE();
+        _clk_enabled[1] = true;
+        break;
+    default:
+        fatal_error("invalid ADC module");
+        break;
     }
 }
 
@@ -60,22 +82,22 @@ void Module::add_internal_channel(ChannelConfig channel_config) {
 } // namespace mcu
 
 
-void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* handle) {
-    using namespace mcu::adc;
-    Module::instance(impl::to_peripheral(handle->Instance))->on_half_completed();
-}
+// void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* handle) {
+//     using namespace mcu::adc;
+//     Module::instance(impl::to_peripheral(handle->Instance))->on_half_completed();
+// }
 
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* handle) {
-    using namespace mcu::adc;
-    Module::instance(impl::to_peripheral(handle->Instance))->on_completed();
-}
+// void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* handle) {
+//     using namespace mcu::adc;
+//     Module::instance(impl::to_peripheral(handle->Instance))->on_completed();
+// }
 
 
-void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *handle) {
-    using namespace mcu::adc;
-    Module::instance(impl::to_peripheral(handle->Instance))->on_error();
-}
+// void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *handle) {
+//     using namespace mcu::adc;
+//     Module::instance(impl::to_peripheral(handle->Instance))->on_error();
+// }
 
 
 #endif
--- stm32/h7/adc/adc.h
+++ stm32/h7/adc/adc.h
@@ -9,6 +9,7 @@
 #include "../system/system.h"
 #include "../gpio/gpio.h"
 #include "../dma/dma.h"
+#include <initializer_list>
 #include <utility>
 
 
@@ -16,9 +17,6 @@ namespace mcu {
 
 
 namespace adc {
-
-
-inline constexpr bool strict_error_check = true;
 
 
 enum class Peripheral : unsigned int {
@@ -38,13 +36,26 @@ struct PinConfig {
 
 
 struct Config {
-    ADC_InitTypeDef hal_init;
-    //ADC_ChannelConfTypeDef channel; 
+    ADC_InitTypeDef hal_config;
 };
 
 
-struct ChannelConfig {
-    ADC_ChannelConfTypeDef hal_init;
+struct RegularChannelConfig {
+    ADC_ChannelConfTypeDef hal_config;
+    std::initializer_list<uint32_t> ranks;
+};
+
+
+struct InjectedChannelConfig {
+    ADC_InjectionConfTypeDef hal_config;
+};
+
+
+enum class InjectedChannelRank {
+    rank1 = ADC_INJECTED_RANK_1,
+    rank2 = ADC_INJECTED_RANK_2,
+    rank3 = ADC_INJECTED_RANK_3,
+    rank4 = ADC_INJECTED_RANK_4
 };
 
 
@@ -54,11 +65,15 @@ namespace impl {
 inline const std::array<ADC_TypeDef*, peripheral_count> adc_instances = {ADC1, ADC2, ADC3};
 
 
+inline const std::array<ADC_Common_TypeDef*, peripheral_count> adc_common_instances = {ADC12_COMMON, ADC12_COMMON, ADC3_COMMON};
+
+
 inline Peripheral to_peripheral(const ADC_TypeDef* instance) {
     return static_cast<Peripheral>(
         std::distance(adc_instances.begin(), std::find(adc_instances.begin(), adc_instances.end(), instance))
     );
 }
+
 
 } // namespace impl
 
@@ -66,75 +81,122 @@ inline Peripheral to_peripheral(const ADC_TypeDef* instance) {
 class Module : public emb::interrupt_invoker_array<Module, peripheral_count>, private emb::noncopyable {
 private:
     const Peripheral _peripheral;
-    ADC_HandleTypeDef _handle = {};
-    static inline std::array<bool, 2> _clk_enabled = {false, false};
+    ADC_HandleTypeDef _handle{};
+    ADC_TypeDef* const _reg;
+    ADC_Common_TypeDef* const _reg_common;
+    static inline std::array<bool, 2> _clk_enabled{false, false};
 public:
-    Module(Peripheral peripheral, const Config& config);
+    Module(Peripheral peripheral, const Config& config, dma::Stream* dma = nullptr);
+    
     Peripheral peripheral() const { return _peripheral; }
     ADC_HandleTypeDef* handle() { return &_handle; }
+    ADC_TypeDef* reg() { return _reg; }
+    ADC_Common_TypeDef* reg_common() { return _reg_common; }
+
     static Module* instance(Peripheral peripheral) {
         return emb::interrupt_invoker_array<Module, peripheral_count>::instance(std::to_underlying(peripheral));
     }
 
     void calibrate();
-    void add_regular_channel(PinConfig pin_config, ChannelConfig channel_config);
-    void add_internal_channel(ChannelConfig channel_config);
+    void initialize_injected_channel(const PinConfig& pin_config, InjectedChannelConfig channel_config);
+    void initialize_regular_channel(const PinConfig& pin_config, const RegularChannelConfig& channel_config);
+    void initialize_injected_internal_channel(InjectedChannelConfig channel_config);
+    void initialize_regular_internal_channel(RegularChannelConfig channel_config);
 
-    void enable_dma(dma::StreamController& dma_stream) {
-        _handle.DMA_Handle = dma_stream.handle();
-        dma_stream.handle()->Parent = &_handle;
-    }
-
-    DrvStatus start_regular_conversion() {
-        DrvStatus status = static_cast<DrvStatus>(HAL_ADC_Start(&_handle));
-        if constexpr (strict_error_check) {
-            if (status != DrvStatus::ok) {
-                fatal_error("regular ADC conversion start failed");
-            }
+    void start_injected() {
+        if (bit_is_set<uint32_t>(_reg->CR, ADC_CR_JADSTART)) {
+            return; // there is ongoing injected channel conversion
         }
-        return status;
+        set_bit<uint32_t>(_reg->CR, ADC_CR_JADSTART);
     }
 
-    template <uint32_t DmaBufSize>
-    DrvStatus start_regular_conversion_with_dma(mcu::dma::Buffer<uint16_t, DmaBufSize>& buf) {
-        DrvStatus status = static_cast<DrvStatus>(
-                HAL_ADC_Start_DMA(&_handle, reinterpret_cast<uint32_t*>(buf.data()), buf.size()));
-        if constexpr (strict_error_check) {
-            if (status != DrvStatus::ok) {
-                fatal_error("regular ADC conversion with DMA start failed");
-            }
+    uint32_t read_injected(InjectedChannelRank rank) {
+        switch (rank) {
+        case InjectedChannelRank::rank1:
+            return _reg->JDR1;
+        case InjectedChannelRank::rank2:
+            return _reg->JDR2;
+        case InjectedChannelRank::rank3:
+            return _reg->JDR3;
+        case InjectedChannelRank::rank4:
+            return _reg->JDR4;
         }
-        return status;
+        return 0xFFFFFFFF;
+    } 
+
+    void acknowledge_injected_conversion() {
+        set_bit<uint32_t>(_reg->ISR, ADC_ISR_JEOC);
     }
 
-    DrvStatus poll() { return static_cast<DrvStatus>(HAL_ADC_PollForConversion(&_handle, 0)); }
-    uint32_t read_regular_conversion() { return HAL_ADC_GetValue(&_handle); }
-    uint32_t read_injected_conversion(uint32_t injected_rank) { return HAL_ADCEx_InjectedGetValue(&_handle, injected_rank); }
+    void acknowledge_injected_sequence() {
+        set_bit<uint32_t>(_reg->ISR, ADC_ISR_JEOS);
+    }
 
-    /* INTERRUPTS */
-public:
-    void (*on_half_completed)() = [](){ fatal_error("uninitialized callback"); };
-    void (*on_completed)() = [](){ fatal_error("uninitialized callback"); };
-    void (*on_error)() = [](){ fatal_error("uninitialized callback"); };
+    void start_regular() {
+        if (bit_is_set<uint32_t>(_reg->CR, ADC_CR_ADSTART)) {
+            return; // there is ongoing regular channel conversion
+        }
+        set_bit<uint32_t>(_reg->CR, ADC_CR_ADSTART);
+    }
+
+    bool busy() const {
+        return bit_is_set<uint32_t>(_reg->CR, ADC_CR_ADSTART);
+    }
+
+    bool regular_ready() const {
+        return bit_is_set<uint32_t>(_reg->ISR, ADC_ISR_EOC);
+    }
+
+    uint32_t read_regular() {
+        return _reg->DR;
+    }
+
+    void acknowledge_regular_conversion() {
+        set_bit<uint32_t>(_reg->ISR, ADC_ISR_EOC);
+    }
+
+    void acknowledge_regular_sequence() {
+        set_bit<uint32_t>(_reg->ISR, ADC_ISR_EOS);
+    }
+
+//     void enable_dma(dma::StreamController& dma_stream) {
+//         _handle.DMA_Handle = dma_stream.handle();
+//         dma_stream.handle()->Parent = &_handle;
+//     }
+
+    // DrvStatus start_regular_conversion() {
+    //     DrvStatus status = static_cast<DrvStatus>(HAL_ADC_Start(&_handle));
+    //     if constexpr (strict_error_check) {
+    //         if (status != DrvStatus::ok) {
+    //             fatal_error("regular ADC conversion start failed");
+    //         }
+    //     }
+    //     return status;
+    // }
+
+//     template <uint32_t DmaBufSize>
+//     DrvStatus start_regular_conversion_with_dma(mcu::dma::Buffer<uint16_t, DmaBufSize>& buf) {
+//         DrvStatus status = static_cast<DrvStatus>(
+//                 HAL_ADC_Start_DMA(&_handle, reinterpret_cast<uint32_t*>(buf.data()), buf.size()));
+//         if constexpr (strict_error_check) {
+//             if (status != DrvStatus::ok) {
+//                 fatal_error("regular ADC conversion with DMA start failed");
+//             }
+//         }
+//         return status;
+//     }
+
+//     DrvStatus poll() { return static_cast<DrvStatus>(HAL_ADC_PollForConversion(&_handle, 0)); }
+//     uint32_t read_regular_conversion() { return HAL_ADC_GetValue(&_handle); }
+//     uint32_t read_injected_conversion(uint32_t injected_rank) { return HAL_ADCEx_InjectedGetValue(&_handle, injected_rank); }
+
+//     /* INTERRUPTS */
+// public:
+//     void (*on_half_completed)() = [](){ fatal_error("uninitialized callback"); };
+//     void (*on_completed)() = [](){ fatal_error("uninitialized callback"); };
+//     void (*on_error)() = [](){ fatal_error("uninitialized callback"); };
 protected:
-    void enable_clk() {
-        switch (_peripheral) {
-        case Peripheral::adc1:
-        case Peripheral::adc2:
-            if (_clk_enabled[0]) return;
-            __HAL_RCC_ADC12_CLK_ENABLE();
-            _clk_enabled[0] = true;
-            break;
-        case Peripheral::adc3:
-            if (_clk_enabled[1]) return;
-            __HAL_RCC_ADC3_CLK_ENABLE();
-            _clk_enabled[1] = true;
-            break;
-        default:
-            fatal_error("invalid ADC module");
-            break;
-        }
-    }
+    static void _enable_clk(Peripheral peripheral);
 };
 
 
