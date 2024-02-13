6b86afc93de037d3cb3604b26ecbd0025e9bb51e
Oleg Aushev <aushevom@gmail.com>
2024-02-13 23:55:51 +0300

    add h7 dma interrupt init

--- stm32/h7/dma/dma.cpp
+++ stm32/h7/dma/dma.cpp
@@ -77,29 +77,5 @@ void Stream::init_interrupts(uint32_t interrupt_list,mcu::IrqPriority priority) 
 } // namespace mcu
 
 
-// extern "C" void DMA1_Stream0_IRQHandler(void) {
-//     using namespace mcu::dma;
-//     HAL_DMA_IRQHandler(StreamController::instance(Stream::dma1_stream0)->peripheral_handle());
-// }
-
-
-// extern "C" void DMA1_Stream1_IRQHandler(void) {
-//     using namespace mcu::dma;
-//     HAL_DMA_IRQHandler(StreamController::instance(Stream::dma1_stream1)->peripheral_handle());
-// }
-
-
-// extern "C" void DMA1_Stream2_IRQHandler(void) {
-//     using namespace mcu::dma;
-//     HAL_DMA_IRQHandler(StreamController::instance(Stream::dma1_stream2)->peripheral_handle());
-// }
-
-
-// extern "C" void DMA1_Stream3_IRQHandler(void) {
-//     using namespace mcu::dma;
-//     HAL_DMA_IRQHandler(StreamController::instance(Stream::dma1_stream3)->peripheral_handle());
-// }
-
-
 #endif
 #endif
