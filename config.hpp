bcf61e79831cc392b5a47187cab32c096d9a9161
Oleg Aushev <aushevom@gmail.com>
2025-02-10 11:48:08 +0300

    c28x: format adc

--- config.hpp
+++ config.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-
-#if !(defined MCUDRV_C28X) && !(defined MCUDRV_STM32) && !(defined MCUDRV_APM32) && !(defined MCUDRV_GD32)
-#error "mcudrv error: mcu is not defined!"
+#if !(defined MCUDRV_C28X)  && \
+    !(defined MCUDRV_STM32) && \
+    !(defined MCUDRV_APM32) && \
+    !(defined MCUDRV_GD32)
+#error "mcudrv error: mcu is not specified!"
 #endif
