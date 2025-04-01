dcb6f813a247ec8e90eda540c1824a51e56276a0
Oleg Aushev <aushevom@gmail.com>
2025-04-01 19:54:21 +0300

    fix type names

--- stm32/h7/can/can.h
+++ stm32/h7/can/can.h
@@ -1,36 +1,25 @@
 #pragma once
 
-
 #ifdef MCUDRV_STM32
 #ifdef STM32H7xx
 
-
-#include <mcudrv/stm32/h7/stm32_h7_base.h>
-#include <mcudrv/stm32/h7/system/system.h>
-#include <mcudrv/stm32/h7/gpio/gpio.h>
 #include <emblib/core.hpp>
 #include <emblib/interfaces/can.h>
+#include <mcudrv/stm32/h7/gpio/gpio.h>
+#include <mcudrv/stm32/h7/stm32_h7_base.h>
+#include <mcudrv/stm32/h7/system/system.h>
 #include <optional>
 #include <utility>
 
-
 namespace mcu {
-
 
 namespace can {
 
-
 inline constexpr bool strict_error_check = true;
 
-
-enum class Peripheral : unsigned int {
-    fdcan1,
-    fdcan2
-};
-
+enum class Peripheral : unsigned int { fdcan1, fdcan2 };
 
 constexpr size_t peripheral_count = 2;
-
 
 struct RxPinConfig {
     GPIO_TypeDef* port;
@@ -38,65 +27,50 @@ struct RxPinConfig {
     uint32_t af_selection;
 };
 
-
 struct TxPinConfig {
     GPIO_TypeDef* port;
     uint32_t pin;
     uint32_t af_selection;
 };
 
-
 struct Config {
     FDCAN_InitTypeDef hal_config;
 };
 
-
 namespace impl {
 
-
-inline constexpr std::array<IRQn_Type, 2> irq0_numbers = {	
+inline constexpr std::array<IRQn_Type, 2> irq0_numbers = {
     FDCAN1_IT0_IRQn,
     FDCAN2_IT0_IRQn,
 };
 
-
-inline constexpr std::array<IRQn_Type, 2> irq1_numbers = {	
+inline constexpr std::array<IRQn_Type, 2> irq1_numbers = {
     FDCAN1_IT1_IRQn,
     FDCAN2_IT1_IRQn,
 };
 
-
-inline const std::array<FDCAN_GlobalTypeDef*, peripheral_count> can_instances = {FDCAN1, FDCAN2};
-
+inline const std::array<FDCAN_GlobalTypeDef*, peripheral_count> can_instances =
+        {FDCAN1, FDCAN2};
 
 inline Peripheral to_peripheral(const FDCAN_GlobalTypeDef* instance) {
-    return static_cast<Peripheral>(
-        std::distance(can_instances.begin(), std::find(can_instances.begin(), can_instances.end(), instance))
-    );
+    return static_cast<Peripheral>(std::distance(
+            can_instances.begin(),
+            std::find(can_instances.begin(), can_instances.end(), instance)));
 }
 
-
-inline constexpr std::array<uint32_t, 9> data_len_codes = {	
-    FDCAN_DLC_BYTES_0,
-    FDCAN_DLC_BYTES_1,
-    FDCAN_DLC_BYTES_2,
-    FDCAN_DLC_BYTES_3,
-    FDCAN_DLC_BYTES_4,
-    FDCAN_DLC_BYTES_5,
-    FDCAN_DLC_BYTES_6,
-    FDCAN_DLC_BYTES_7,
-    FDCAN_DLC_BYTES_8
-};
-
+inline constexpr std::array<uint32_t, 9> data_len_codes = {FDCAN_DLC_BYTES_0,
+                                                           FDCAN_DLC_BYTES_1,
+                                                           FDCAN_DLC_BYTES_2,
+                                                           FDCAN_DLC_BYTES_3,
+                                                           FDCAN_DLC_BYTES_4,
+                                                           FDCAN_DLC_BYTES_5,
+                                                           FDCAN_DLC_BYTES_6,
+                                                           FDCAN_DLC_BYTES_7,
+                                                           FDCAN_DLC_BYTES_8};
 
 } // namespace impl
 
-
-enum class RxFifo {
-    fifo0,
-    fifo1
-};
-
+enum class RxFifo { fifo0, fifo1 };
 
 struct RxMessageAttribute {
     uint32_t storage{0xBAAAAAAD};
@@ -105,13 +79,15 @@ struct RxMessageAttribute {
     bool operator==(const RxMessageAttribute&) const = default;
 };
 
-
-class Module : public emb::singleton_array<Module, peripheral_count>, private emb::noncopyable {
+class Module : public emb::singleton_array<Module, peripheral_count>,
+               private emb::noncopyable {
     friend void ::HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef*, uint32_t);
     friend void ::HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef*, uint32_t);
-    friend void ::HAL_FDCAN_RxBufferNewMessageCallback(FDCAN_HandleTypeDef *hfdcan);
-    friend void ::HAL_FDCAN_ErrorStatusCallback(FDCAN_HandleTypeDef *hfdcan, uint32_t ErrorStatusITs);
-    friend void ::HAL_FDCAN_ErrorCallback(FDCAN_HandleTypeDef *hfdcan);
+    friend void ::HAL_FDCAN_RxBufferNewMessageCallback(
+            FDCAN_HandleTypeDef* hfdcan);
+    friend void ::HAL_FDCAN_ErrorStatusCallback(FDCAN_HandleTypeDef* hfdcan,
+                                                uint32_t ErrorStatusITs);
+    friend void ::HAL_FDCAN_ErrorCallback(FDCAN_HandleTypeDef* hfdcan);
 private:
     const Peripheral _peripheral;
     FDCAN_HandleTypeDef _handle = {};
@@ -130,13 +106,17 @@ private:
     uint32_t _error_status_count{0};
     uint32_t _error_count{0};
 public:
-    Module(Peripheral peripheral, const RxPinConfig& rx_pin_config, const TxPinConfig& tx_pin_config, const Config& config);
+    Module(Peripheral peripheral,
+           const RxPinConfig& rx_pin_config,
+           const TxPinConfig& tx_pin_config,
+           const Config& config);
     RxMessageAttribute register_message(FDCAN_FilterTypeDef& filter);
-    
+
     Peripheral peripheral() const { return _peripheral; }
     FDCAN_HandleTypeDef* handle() { return &_handle; }
     static Module* instance(Peripheral peripheral) {
-        return emb::singleton_array<Module, peripheral_count>::instance(std::to_underlying(peripheral));
+        return emb::singleton_array<Module, peripheral_count>::instance(
+                std::to_underlying(peripheral));
     }
 
     void start() {
@@ -171,24 +151,23 @@ public:
             .BitRateSwitch = FDCAN_BRS_OFF,
             .FDFormat = FDCAN_CLASSIC_CAN,
             .TxEventFifoControl = FDCAN_NO_TX_EVENTS,
-            .MessageMarker = 0
-        };
+            .MessageMarker = 0};
 
-        DrvStatus status = static_cast<DrvStatus>(
-                HAL_FDCAN_AddMessageToTxFifoQ(&_handle, &header, frame.payload.data()));
+        DrvStatus status = static_cast<DrvStatus>(HAL_FDCAN_AddMessageToTxFifoQ(
+                &_handle, &header, frame.payload.data()));
         if (status != DrvStatus::ok) {
             ++_tx_error_counter;
         }
         return status;
     }
 
-    DrvStatus put_frame(FDCAN_TxHeaderTypeDef& header, can_payload& payload) {
-        DrvStatus status = static_cast<DrvStatus>(
-                HAL_FDCAN_AddMessageToTxFifoQ(&_handle, &header, payload.data()));
+    DrvStatus put_frame(FDCAN_TxHeaderTypeDef& header, canpayload_t& payload) {
+        DrvStatus status = static_cast<DrvStatus>(HAL_FDCAN_AddMessageToTxFifoQ(
+                &_handle, &header, payload.data()));
         if (status != DrvStatus::ok) {
             ++_tx_error_counter;
         }
-        return status;		
+        return status;
     }
 
     std::optional<RxMessageAttribute> get_frame(can_frame& frame, RxFifo fifo);
@@ -198,32 +177,49 @@ public:
 
     /* INTERRUPTS */
 private:
-    void (*_on_fifo0_frame_received)(Module&, const RxMessageAttribute&, const can_frame&) = [](auto, auto, auto){ fatal_error("uninitialized callback"); };
-    void (*_on_fifo1_frame_received)(Module&, const RxMessageAttribute&, const can_frame&) = [](auto, auto, auto){ fatal_error("uninitialized callback"); };
-    void (*_on_buffer_frame_received)() = [](){ fatal_error("uninitialized callback"); };
-    void (*_on_error_status)(Module&, uint32_t error_status) = [](auto, auto){};
-    void (*_on_bus_off)(Module&) = [](auto){};
-    void (*_on_error)(Module&) = [](auto){};
+    void (*_on_fifo0_frame_received)(Module&,
+                                     const RxMessageAttribute&,
+                                     const can_frame&) = [](auto, auto, auto) {
+        fatal_error("uninitialized callback");
+    };
+    void (*_on_fifo1_frame_received)(Module&,
+                                     const RxMessageAttribute&,
+                                     const can_frame&) = [](auto, auto, auto) {
+        fatal_error("uninitialized callback");
+    };
+    void (*_on_buffer_frame_received)() = []() {
+        fatal_error("uninitialized callback");
+    };
+    void (*_on_error_status)(Module&, uint32_t error_status) = [](auto, auto) {
+    };
+    void (*_on_bus_off)(Module&) = [](auto) {};
+    void (*_on_error)(Module&) = [](auto) {};
 public:
-    void register_on_fifo0_frame_received_callback(void(*callback)(Module&, const RxMessageAttribute&, const can_frame&)) {
+    void register_on_fifo0_frame_received_callback(void (*callback)(
+            Module&, const RxMessageAttribute&, const can_frame&)) {
         _on_fifo0_frame_received = callback;
     }
-    void register_on_fifo1_frame_received_callback(void(*callback)(Module&, const RxMessageAttribute&, const can_frame&)) {
+    void register_on_fifo1_frame_received_callback(void (*callback)(
+            Module&, const RxMessageAttribute&, const can_frame&)) {
         _on_fifo1_frame_received = callback;
     }
-    void register_on_buffer_frame_received_callback(void(*callback)()) {
+    void register_on_buffer_frame_received_callback(void (*callback)()) {
         _on_buffer_frame_received = callback;
     }
-    void register_on_bus_off_callback(void(*callback)(Module&)) {
+    void register_on_bus_off_callback(void (*callback)(Module&)) {
         _on_bus_off = callback;
     }
 
-    void initialize_interrupts(uint32_t interrupt_list, uint32_t interrupt_line);
+    void initialize_interrupts(uint32_t interrupt_list,
+                               uint32_t interrupt_line);
     void set_fifo_watermark(uint32_t fifo, uint32_t watermark);
 
-    void set_interrupt_priority(IrqPriority line0_priority, IrqPriority line1_priority) {
-        set_irq_priority(impl::irq0_numbers[std::to_underlying(_peripheral)], line0_priority);
-        set_irq_priority(impl::irq1_numbers[std::to_underlying(_peripheral)], line1_priority);
+    void set_interrupt_priority(IrqPriority line0_priority,
+                                IrqPriority line1_priority) {
+        set_irq_priority(impl::irq0_numbers[std::to_underlying(_peripheral)],
+                         line0_priority);
+        set_irq_priority(impl::irq1_numbers[std::to_underlying(_peripheral)],
+                         line1_priority);
     }
 
     void enable_interrupts() {
@@ -237,18 +233,16 @@ public:
     }
 protected:
     void enable_clk() {
-        if (_clk_enabled) return;
+        if (_clk_enabled)
+            return;
         __HAL_RCC_FDCAN_CLK_ENABLE();
         _clk_enabled = true;
     }
 };
 
-
 } // namespace can
 
-
 } // namespace mcu
-
 
 #endif
 #endif
